9df7dd224f00067919a507b2ca327bac6fed66fd
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-03 15:35:41 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -13,6 +13,6 @@ int array_sum(int array[], int n){
 }
 int main() {
     int new_array[6] = {1,2,-4,6,8,3};
-    cout << array_sum(new_array, 3) << endl;
+    cout << array_sum(new_array, 6) << endl;
     return 0;
 }
