2dda7c13f9de560646d7481e960896125e54bcc3
sara <farooqisara@gmail.com>
2025-08-03 19:55:08 +0930

    change new

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -4,5 +4,10 @@ bool is_ascending(int array[], int n){
     if (n < 1){
         return false;
     }
-    return std::is_sorted(array, array + n);
+    for (int i = 1; i < n; i++){
+        if (array[i] < array [i-1]){
+            return false;
+        }
+    }
+    return true;
 }
