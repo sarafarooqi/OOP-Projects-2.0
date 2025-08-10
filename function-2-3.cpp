c2ce84c277d734ce54096476ecff3103725d29bb
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-10 14:46:23 +0930

    Update function-2-3.cpp

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -33,13 +33,3 @@ int sum_if_palindrome(int integers[], int length){
     }
 }
 
-bool is_palindrome(int integers[], int length);
-int sum_array_elements(int integers[], int length);
-int sum_if_palindrome(int integers[], int length);
-
-int main(){
-    int array1[] = {1,2,2,1};
-    cout<<sum_if_palindrome(array1,4)<<endl;
-
-}
-
