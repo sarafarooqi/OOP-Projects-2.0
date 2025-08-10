e2c54db0013663cfb8a41360bf3c23f9715d3170
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-10 14:50:54 +0930

    Update function-2-3.cpp

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -5,6 +5,9 @@ using namespace std;
 bool is_palindrome(int integers[], int length){
     int start = 0;
     int end = length-1;
+    if(length <= 0){
+        return false;
+    }
     while(start < end){
         if(integers[start] != integers[end]){
             return false;
@@ -16,6 +19,9 @@ bool is_palindrome(int integers[], int length){
 }
 
 int sum_array_elements(int integers[], int length){
+    if(length<=0){
+        return -1;
+    }
     int total = 0;
     for(int i = 0; i < length; i++){
         total = total + integers[i];
@@ -25,6 +31,9 @@ int sum_array_elements(int integers[], int length){
 
 
 int sum_if_palindrome(int integers[], int length){
+    if(length<=0){
+        return -1;
+    }
     if(is_palindrome(integers, length)){
         return sum_array_elements(integers, length);
     }
