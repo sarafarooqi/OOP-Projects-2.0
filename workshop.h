6262001f7d5b8201735059913cb5aa4a38258a65
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-12 22:09:08 +0930

    Update workshop.h

--- workshop.h
+++ workshop.h
@@ -2,24 +2,16 @@
 using namespace std;
 
 //Part 2
-void changeValue(double* p){
-    *p = 42;
-}
-
-void numbers_1(){
+void changeValue(double* nums){
+    *nums = 42;
     double num;
 
-    cout << "Enter a number: ";
-    cin >> num;
+    nums = &num;
+}
 
-    double *n;
-
-    n = &num;
-
-    changeValue(n);
-
-    cout<<"Value of num: "<<*n<<endl;
-} 
+//When passing a pointer to a function, it changes the value of the original variable that the pointer points to
+/*If you passed just a double, the function would take in a copy of the variable. It wouldn't change the original 
+value if it exists outside the function.*/
 
 //Part 3
 void printArray(double* arr, int size_arr){
@@ -29,35 +21,42 @@ void printArray(double* arr, int size_arr){
     cout<<endl;
 }
 
+/*You can pass an array as a function in c++ by passing it as a pointer because the pointer 
+points to the address of only the element at index 0. The size is needed because the function needs to know
+how big the array is.*/
+
 //Part 4
 double arrayMax(double* array, int size){
-    if (size <= 0){
-        return -1;
-    }
     double max = array[0];
     for (int i = 1; i < size; i++){
         if (array[i] > max){
             max = array[i];
         }
-    return max;
     }
+    return max;
 }
+/*The reason it returns a double is because the code asks for the max value in the array, 
+it doesn't ask for the address of the largest value in the array. */
 
 //Part 5
-double* dynamicArray(int n){
-    double* array1 = new double[n];
-    for(int i = 0; i < n; i++){
+double* dynamicArray(int N){
+    double* array1 = new double[N];
+    for(int i = 0; i < N; i++){
         array1[i] = i;
     }
     return array1;
 }
+/*We have to return a pointer from the function because in c++, the array can't be returned by value,
+it needs the address and the pointer gives that. */
 
 //Part 7
-double* dynamicArray(int n, double m){
-    double* array1 = new double[n];
+double* dynamicArray(int N, double M){
+    double* array1 = new double[N];
 
-    for(int i = 0; i < n; i++){
-        array1[i] = m;
+    for(int i = 0; i < N; i++){
+        array1[i] = M;
     }
     return array1;
 }
+/*When initializing M, it gets rid of any filler values that the program filled the array with and the practical 
+implication is that the array will have meaningful and predictable values that user can use.*/
