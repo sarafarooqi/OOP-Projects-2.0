58c70a9f5d5b23381cdb988b8a7167a29401c986
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-13 15:56:52 +0930

    Update main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 using namespace std;
 
-double array_mean(int array[], int n){
-    int total = 0;
-    if (n < 1){
-        return 0;
-    }
-    for (int i = 0; i < n; i++){
-        total = total + array[i];
-    }
-   double avg = static_cast<double>(total)/n;
-    return avg;
-}
+double array_mean(int array[], int n);
+
 int main() {
     int new_array[6] = {1,2,-4,6,8,3};
     cout << array_mean(new_array, 6) << endl;
