bc9e1941c998950e13a528c156e8282d1bbf6cf4
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-13 15:56:40 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int array_sum(int array[], int n){
-    if (n < 1){
-        return 0;
-    }
-    int total = 0;
-    for (int i = 0; i < n; i++){
-        total += array[i];
-    }
-    return total;  
-}
+int array_sum(int array[], int n);
 int main() {
     int new_array[6] = {1,2,-4,6,8,3};
     cout << array_sum(new_array, 3) << endl;
