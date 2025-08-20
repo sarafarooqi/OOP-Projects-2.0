659e193ffc7b53cb60545b17f856c48b94ddf13e
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-20 17:27:09 +0930

    Create function-1-1.cpp

---
+++ function-1-1.cpp
@@ -0,0 +1,14 @@
+int *readNumbers(){
+    int *array = new int[10];
+    for(int i = 0; i < 10; i++){
+        std::cin >> array[i];
+    }
+    return array;
+}
+
+void printNumbers(int *numbers, int length){
+    for(int i = 0; i < length; i++){
+        std::cout << i << " ";
+        std::cout << numbers[i] << " " << std::endl;
+    }
+}
