327b7f49d190dcab9df575679c66894bd59ff96e
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-24 18:30:45 +0930

    Update main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -2,12 +2,11 @@
 #include "Person.h"
 
 PersonList createPersonList(int n);
-
 int main(void){
     int n = 4;
     PersonList arr1 = createPersonList(n);
     for(int i = 0; i < n; i++){
-        std::cout << arr1.people[i].name << " " << arr1.people[i].age <<std::endl;
+        std::cout << "Person " << i+1 << ": " <<arr1.people[i].name << ", " << arr1.people[i].age <<std::endl;
     }
     delete[] arr1.people;
     return 0;
