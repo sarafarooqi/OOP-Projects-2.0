aa408685165d7b29a01e3a9efa021b332b72f9e7
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-24 18:16:17 +0930

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include "Person.h"
 
+
 Person* createPersonArray(int n){
     Person* array = new Person[n];
     for(int i = 0; i < n; i++){
         array[i].name = "John doe";
         array[i].age = 0;
     }
+
+    for(int i = 0; i < n; i++){
+        std::cout << "Person " << i+1 << ": " << array[i].name << ", " << array[i].age <<std::endl;
+    }
     return array;
 
 }
+
