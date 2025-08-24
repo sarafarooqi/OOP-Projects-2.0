4c40ec37e7cf078cb75518aff24b20040056b86d
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-24 16:31:12 +0930

    Create main-1-3.cpp

---
+++ main-1-3.cpp
@@ -0,0 +1,21 @@
+#include "Person.h"
+#include <iostream>
+
+PersonList deepCopyPersonList(PersonList pl);
+
+int main(void){
+    PersonList old = createPersonList(4);
+    PersonList new1 = deepCopyPersonList(old);
+
+
+   new1.people[2].name = "Jenny";
+   new1.people[1].age = 2;
+
+   for(int i = 1; i < 4; i++){
+    std::cout << new1.people[i].name << " " << new1.people[i].age << std::endl;
+   }
+
+   delete[] old.people;
+   delete[] new1.people;
+   return 0;
+}
