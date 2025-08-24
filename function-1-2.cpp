8135f19cb4e3045ac119254c10b8cb0e0c93368f
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-24 18:31:15 +0930

    Update function-1-2.cpp

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -7,7 +7,7 @@ PersonList createPersonList(int n){
     new_array.numPeople = n;
     new_array.people = new Person[n];
     for(int i = 0; i < n; i++){
-        new_array.people[i].name = "Jane doe";
+        new_array.people[i].name = "Jane Doe";
         new_array.people[i].age = 1;
     }
     return new_array;
