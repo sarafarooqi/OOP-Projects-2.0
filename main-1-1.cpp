be43ee3974caf5a0fdeb4fe96cf3727ae5d28005
sarafarooqi <a1908352@adelaide.edu.au>
2025-08-24 16:26:31 +0930

    Create main-1-1.cpp

---
+++ main-1-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Person.h"
+Person* createPersonArray(int n);
+int main(void){
+    /* Person person1;
+    person1.name = "The University";
+    person1.age = 150;
+
+    std::cout << "Person(name: " << person1.name << " , age: " << person1.age << ")" << std::endl;
+
+    Person* person2 = new Person;
+    person2-> name = "Adelaide";
+    person2 -> age = 0;
+    std::cout << "Person2(name: " << person2->name << " , age: " << person1.age << ")" << std::endl;
+ */
+
+    int n = 4;
+    Person* point = createPersonArray(n); 
+    for(int i = 0; i < n; i++){
+        std::cout << point[i].name << " " << point[i].age <<std::endl;
+    }
+    delete[] point;
+    return 0;
+}
