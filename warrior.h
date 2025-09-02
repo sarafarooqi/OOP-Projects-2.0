3e04b8655434a24ac2cdccd96ded862b1c29edc2
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-02 11:17:45 +0930

    Create warrior.h

---
+++ warrior.h
@@ -0,0 +1,18 @@
+#ifndef WARRIOR_H
+#define WARRIOR_H
+#include "player.h"
+
+class Warrior: public Player{
+    public:
+    string weapon;
+
+    Warrior(string name, int health, int damage, string weapon)
+    :Player (name, health, damage){
+    }
+    
+    //deals damage to opponent based on the player's mana points
+    void swingWeapon(Player* opponent);
+};
+
+
+#endif
