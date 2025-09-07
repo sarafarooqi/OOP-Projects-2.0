3611d01127f55c79156241786e495f4663ae3168
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-07 22:40:55 +0930

    Create ParkingLot.h

---
+++ ParkingLot.h
@@ -0,0 +1,20 @@
+#ifndef PARKINGLOT_H
+#define PARKINGLOT_H
+#include "Vehicle.h"
+#include <iostream>
+using namespace std;
+
+class ParkingLot {
+protected:
+    int maxSpaces;
+    Vehicle** vehicles;
+    int count;
+
+public:
+    ParkingLot(int maxSpaces);
+    int getCount();
+    void parkVehicle(Vehicle* types);
+    void unparkVehicle(int ID);
+};
+
+#endif
