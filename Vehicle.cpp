1c5b42a62facfadc1371d978adf6b02d24998f0c
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-07 20:48:57 +0930

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,13 +1,10 @@
 #include "Vehicle.h"
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
-
-Vehicle::Vehicle(int ID){
-    this -> timeOfEntry = time(0);
-    this -> ID = ID;
-}
-int Vehicle::getID() const{
-    return ID;
+Vehicle::Vehicle(int ID) {
+    this->timeOfEntry = std::time(nullptr);
+    this->ID = ID;
 }
