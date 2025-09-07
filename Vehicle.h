e007dd5eda1c4844965c5c8a6f04cc02ffebb7d4
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-07 19:03:59 +0930

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -13,7 +13,7 @@ class Vehicle{
     public:
     Vehicle(int ID);
     int getID() const;
-    virtual int getParkingDuration(int seconds) = 0;
+    virtual int getParkingDuration(int duration) = 0;
     virtual ~Vehicle() = default;
 };
 #endif
