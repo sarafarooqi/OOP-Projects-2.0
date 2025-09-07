cacf19fc67aa24f1b336d7f0995452e6d5618be8
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-07 23:09:16 +0930

    Update main-1-3.cpp

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,1 +1,23 @@
-#include 
+#include <iostream>
+#include <string>
+#include "Car.h"
+#include "Bus.h"
+#include "Motorbike.h"
+#include "ParkingLot.h"
+using namespace std;
+
+int main(){
+    ParkingLot space(10);
+
+    for(int i = 1; i <=5; i++){
+        space.parkVehicle(new Car(i));
+    }
+    for(int i = 6; i <=8;i++){
+        space.parkVehicle(new Bus(i));
+    }
+    for(int i = 9; i <= 10; i++){
+        space.parkVehicle(new Motorbike(i));
+    }
+
+    cout << "Number of vehicles overstaying: " << space.countOverstayingVehicles(15) << endl;
+}
