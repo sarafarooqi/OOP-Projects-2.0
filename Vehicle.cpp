00fa6323f88a1c295b6e14ae61b9259379d3064e
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-07 18:14:08 +0930

    Create Vehicle.cpp

---
+++ Vehicle.cpp
@@ -0,0 +1,11 @@
+#include "Vehicle.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+Vehicle::Vehicle(string timeOfEntry, int ID){
+    this -> timeOfEntry = timeOfEntry;
+    this -> ID = ID;
+    this -> duration = duration;
+}
