d83ce7d71521fe0db5bb2c85b16454d9a6dff8b3
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-07 22:41:47 +0930

    Create ParkingLot.cpp

---
+++ ParkingLot.cpp
@@ -0,0 +1,38 @@
+#include "ParkingLot.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+ParkingLot::ParkingLot(int maxSpaces) {
+    this->maxSpaces = maxSpaces;
+    this->count = 0;
+    vehicles = new Vehicle*[maxSpaces];
+}
+
+int ParkingLot::getCount() {
+    return count;
+}
+
+void ParkingLot::parkVehicle(Vehicle* types) {
+    if (count >= maxSpaces) {
+        cout << "The lot is full" << endl;
+    }
+    vehicles[count++] = types;
+}
+
+void ParkingLot::unparkVehicle(int ID) {
+    bool temp = false;
+    for (int i = 0; i < count; i++) {
+        if (vehicles[i]->getID() == ID) {
+            for (int j = i; j < count - 1; j++) {
+                vehicles[j] = vehicles[j + 1];
+            }
+            count--;
+            temp = true;
+            break;
+        }
+    }
+    if (temp != false) {
+        cout << "Vehicle is not in the lot" << endl;
+    }
+}
