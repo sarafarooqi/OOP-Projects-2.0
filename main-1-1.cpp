424b976a9a24eac6eb0818da19cc84ce7be12575
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-07 20:53:36 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,43 +1,35 @@
 #include <iostream>
 #include <string>
-#include "Vehicle.h"
 #include "Car.h"
 #include "Bus.h"
 #include "Motorbike.h"
+
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    cout << "Enter the number of vehicles you would like to park:  " << endl;
+    cout << "Enter the number of vehicles you would like to park: " << endl;
     cin >> n;
 
-    Vehicle *vehicles[n];
+    Vehicle* vehicles[n];
     string type;
 
-
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cout << "Enter the type of vehicle: " << endl;
         cin >> type;
 
         int ID = i + 1;
 
-        if (type == "Car"){
+        if (type == "Car") {
             vehicles[i] = new Car(ID);
-        }else if(type == "Bus"){
+        } else if (type == "Bus") {
             vehicles[i] = new Bus(ID);
-        }else{
+        } else {
             vehicles[i] = new Motorbike(ID);
         }
 
-        int seconds;
-        cout << "Enter the duration that the vehicle is parked: ";
-        cin >> seconds;
-
-        vehicles[i]->setTime(seconds);
         cout << vehicles[i]->getParkingDuration() << endl;
     }
-    for (int i = 0; i < n; i++){
-        delete vehicles[i];
-    }
+
     return 0;
 }
