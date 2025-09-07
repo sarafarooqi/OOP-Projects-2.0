c8f51613a602a6099de09895f489b363f025c5dd
sarafarooqi <a1908352@adelaide.edu.au>
2025-09-07 20:50:12 +0930

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,11 +1,14 @@
 #include "Car.h"
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
-Car::Car(int ID):Vehicle(ID){
+Car::Car(int ID) : Vehicle(ID) {
 }
 
-        int Car::getParkingDuration(){
-        return duration - (duration * 0.10)/100;
-        }
+int Car::getParkingDuration() {
+    int duration = (std::time(nullptr) - timeOfEntry);
+    int reduction = (duration * 0.10);
+    return duration - reduction;
+}
