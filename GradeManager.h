ff7c494cee831972826a5671b1dbbf8a4c3bb06d
sarafarooqi <a1908352@adelaide.edu.au>
2025-10-22 17:55:36 +1030

    Create GradeManager.h

---
+++ GradeManager.h
@@ -0,0 +1,33 @@
+#ifndef GRADE_MANAGER
+#define GRADE_MANAGER
+#include <vector>
+#include <string.h>
+
+class GradeManager{
+    private:
+    std::vector<int> grades;
+
+    public:
+    GradeManager();
+
+    void addGrade(int grade);
+
+    void removeAt(int index);
+
+    void updateGrade(int index, int newGrade);
+
+    int countOccurences(int grade);
+
+    std::vector<int> findAllIndices(int grade);
+
+    int countBelow(int threshold);
+
+    int getSize();
+
+    double getAverage();
+
+    void clearGrades();
+
+};
+
+#endif
